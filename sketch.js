7a568ca519f4f43bc56cb39b1ba26dcbb1b584de
juanpa1233238055 <amerikmv@gmail.com>
2024-06-11 19:06:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+var trex ,trex_running;
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //crear sprite de Trex
+ trex createsprite{50,100,20,50}
+
+ trex.scale 0.5
+ground createsprite{}
+
+function draw(){
+  background("white")
+  
+
+}
